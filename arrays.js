71cd8b3ae1cc04c7d9dc02fb02b2ccb2e45f3c05
Sara Park <sspark106@gmail.com>
2022-11-09 16:09:03 -0700

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,4 +1,15 @@
 var drinks = ["tea", "coffee", "water", "milk"];
-var numberOfPets = [0, 1, 2, 3];
+var pets = [0, 1, 2, 3];
 var coldDrink = [true, false, false, true];
 
+//method pop will remove last element
+drinks.pop();
+console.log(drinks);
+
+//method push will add element to end of array
+pets.push(4);
+console.log(pets);
+
+//method shift will remove first element
+coldDrink.shift();
+console.log(coldDrink);
